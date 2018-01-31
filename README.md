5ff88418a793cb9df55901c71a5a851c7c08dc97
Linda Cai <linda.cai@zoho.com>
2018-01-31 00:21:51 -0800

    reorganize style, add missing case

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # content-site
+
+```
+git clone git@github.com:ELinda/content-site.git
+git checkout develop
+
+cd content-site
+alias python=python3
+alias pip=pip3
+python -m venv --without-pip ~/site/
+source ~/site/bin/activate
+sudo pip install -U -r requirements.txt
+wget https://code.jquery.com/jquery-3.1.1.min.js -P static/
+```
--- run.py
+++ run.py
@@ -25,6 +25,9 @@ def get_urls(paths):
                  for path in paths])
 
 def get_all_image_links(notebook_name):
+    """
+    provides a list of dictionaries with information needed for image links
+    """
     base_path = os.path.join('%snotebooks' % STATIC, notebook_name)
     thumbs = get_urls(glob.glob(os.path.join(base_path, 'thumbs/*')))
     images = get_urls(glob.glob(os.path.join(base_path, 'images/*')))
@@ -39,8 +42,9 @@ def get_notebook(book):
     base_path = os.path.join('%snotebooks/%s' % (STATIC, book))
     repr_images = [static_url_from_relative(path) for path in 
                    glob.glob(os.path.join(base_path, '*.png'))]
+    repr_image = repr_images[0] if repr_images and len(repr_images) else None
     image_links = get_all_image_links(book)
-    return render_template('book.html', repr_image=repr_images[0], image_links=image_links)
+    return render_template('book.html', repr_image=repr_image, image_links=image_links)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = False)
